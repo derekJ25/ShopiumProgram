70a84d5d44535c6c4ea2fc6361e5c0ac596a1b7b
derekJ25 <99772914+derekJ25@users.noreply.github.com>
2022-03-14 21:40:08 +1100

    Update Menu.java

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,22 +1,69 @@
 package main;
 
+import java.util.Scanner;
+
 public class Menu {
+	private final Scanner scanner = new Scanner(System.in);
+	private boolean userChoice = true;
 	
 	public void run(){
 		System.out.println("Welcome to Shopium!");
-		runMenu();
+		
+		while (userChoice) {
+			printMenu();
+			String option = scanner.nextLine();
+
+			switch (option) {
+			case "1":
+				searchItem();
+				userChoice = false;
+				break;
+			case "2":
+				browseItem();
+				userChoice = false;
+				break;
+			case "3":
+				addItemToStore();
+				userChoice = false;
+				break;
+			case "4":
+				checkCart();
+				userChoice = false;
+				break;
+			case "5":
+				System.exit(1);
+			default:
+				System.out.println("Please select a valid option.");
+			}
+		}
 	}
 	
-	private void runMenu() {
+	private void searchItem() {
+		System.out.println("Search item.");
+	}
+	
+	private void browseItem() {
+		System.out.println("Browse item.");
+	}
+	
+	private void addItemToStore() {
+		System.out.println("Add item to store.");
+	}
+	
+	private void checkCart() {
+		System.out.println("Viewing cart.");
+	}
+	
+	private void printMenu() {
 		System.out.println("-------------------------------------");
 		System.out.println("> Select from main menu");
 		System.out.println("-------------------------------------");
 		System.out.printf("%3d) %s%n", 1, "Search item");
-		System.out.printf("%3d) %s%n", 2, "Add item");
-		System.out.printf("%3d) %s%n", 3, "Browse Items");
-		System.out.printf("%3d) %s%n", 4, "Checklist");
-		System.out.printf("%3d) %s%n", 5, "Cart");
-		System.out.printf("%3d) %s%n", 6, "Exit");
+		System.out.printf("%3d) %s%n", 2, "Browse Items");
+		System.out.printf("%3d) %s%n", 3, "Add item to store");
+		System.out.printf("%3d) %s%n", 4, "Cart");
+		System.out.printf("%3d) %s%n", 5, "Exit");
 		System.out.println("-------------------------------------");
+		System.out.print("Please select: ");
 	}
 }

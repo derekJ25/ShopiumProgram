963699008e4ad3ec4505d07210b763e0d337e03a
derekJ25 <99772914+derekJ25@users.noreply.github.com>
2022-03-18 11:08:45 +1100

    Update
    Updated some methods in main menu

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -2,13 +2,25 @@ package main;
 
 import java.util.Scanner;
 
+import shopiumSystem.Item;
+import shopiumSystem.ShopiumSystem;
+
 public class Menu {
 	private final Scanner scanner = new Scanner(System.in);
+	private final ShopiumSystem system = new ShopiumSystem();
 	private boolean userChoice = true;
-	
-	public void run(){
+
+	public void run() {
 		System.out.println("Welcome to Shopium!");
-		
+
+		system.addItem(new Item("Fruit", "Apple", 1000, 4.99));
+		system.addItem(new Item("Fruit", "Banana", 1000, 2.99));
+		system.addItem(new Item("Vegetable", "Carrot", 1000, 0.99));
+
+		runMenu();
+	}
+
+	public void runMenu() {
 		while (userChoice) {
 			printMenu();
 			String option = scanner.nextLine();
@@ -31,35 +43,121 @@ public class Menu {
 				userChoice = false;
 				break;
 			case "5":
+				System.out.println("Exiting program.");
 				System.exit(1);
+//	ERROR DETECTION		
 			default:
 				System.out.println("Please select a valid option.");
 			}
 		}
 	}
-	
+
 	private void searchItem() {
-		System.out.println("Search item.");
+		System.out.println("-------------------------------------");
+		System.out.println("> Searching items");
+		System.out.println("-------------------------------------");
+		System.out.print("Search for item: ");
+		String searchKeyword = scanner.nextLine();
+		system.searchItem(searchKeyword);
+		if (system.getSearchedResults().size() == 0) {
+			System.out.printf("%s%n", "There are no items matching your search.");
+			System.out.println("-------------------------------------");
+			System.out.printf("%3d) %s%n", 1, "Search another item");
+			System.out.printf("%3d) %s%n", 2, "Return to menu");
+			System.out.print("Please select: ");
+			String option = scanner.nextLine();
+			if (option.equalsIgnoreCase("1")) {
+				searchItem();
+			} else {
+				runMenu();
+			}
+		} else if (system.getSearchedResults().size() == 1) {
+			System.out.printf("%s%n", "There are 1 item matching your search.");
+		} else {
+			System.out.printf("There are %d items matching your search.%n", system.getSearchedResults().size());
+		}
+//	Not Completed
+		
 	}
-	
+
 	private void browseItem() {
-		System.out.println("Browse item.");
+		System.out.println("-------------------------------------");
+		System.out.println("> Browsing Items");
+		System.out.println("-------------------------------------");
+		System.out.printf("%3d) %s%n", 1, "Browse by name");
+		System.out.printf("%3d) %s%n", 2, "Browse by category");
+		System.out.printf("%3d) %s%n", 3, "Browse by price");
+		System.out.printf("%3d) %s%n", 4, "Browse by weight");
+		System.out.printf("%3d) %s%n", 5, "Return to menu");
+		System.out.println("-------------------------------------");
+		System.out.print("Please select: ");
+		String userInput = scanner.nextLine();
+		switch(userInput) {
+		case "1":
+//			browseName();
+		case "2":
+//			browseCategory();
+		case "3":
+//			browsePrice();
+		case "4":
+//			browseWeight();
+		case "5":
+			runMenu();
+		default:
+			System.out.println("Please select a valid option.");
+			browseItem();
+		}
+//	Not Completed		
+		
 	}
-	
+
 	private void addItemToStore() {
-		System.out.println("Add item to store.");
+		System.out.println("-------------------------------------");
+		System.out.println("> Add item to store");
+		System.out.println("-------------------------------------");
+		System.out.printf("%3d) %s%n", 1, "Add item");
+		System.out.printf("%3d) %s%n", 2, "Add premade items");
+		System.out.printf("%3d) %s%n", 3, "Add from file");
+		System.out.printf("%3d) %s%n", 4, "Return to menu");
+		String userInput = scanner.nextLine();
+		switch(userInput) {
+		case "1":
+//			addItem();
+		case "2":
+//			addPremadeItem();
+		case "3":
+//			addFromFile();
+		case "4":	
+			runMenu();
+		default:
+			System.out.println("Pease enter a valid input.");
+			addItemToStore();
+		}
+//	Not Completed		
+		
 	}
-	
+
 	private void checkCart() {
-		System.out.println("Viewing cart.");
+		System.out.println("-------------------------------------");
+		System.out.println("> Viewing cart");
+		System.out.println("-------------------------------------");
+		if(system.getCartItems().size() == 0) {
+			System.out.println("You have nothing in the cart. \nReturning to menu.");
+			runMenu();
+		} else {
+//	Display the items in cart			
+			
+		}
+//	Not completed		
+		
 	}
-	
+
 	private void printMenu() {
 		System.out.println("-------------------------------------");
 		System.out.println("> Select from main menu");
 		System.out.println("-------------------------------------");
 		System.out.printf("%3d) %s%n", 1, "Search item");
-		System.out.printf("%3d) %s%n", 2, "Browse Items");
+		System.out.printf("%3d) %s%n", 2, "Browse items");
 		System.out.printf("%3d) %s%n", 3, "Add item to store");
 		System.out.printf("%3d) %s%n", 4, "Cart");
 		System.out.printf("%3d) %s%n", 5, "Exit");
--- src/shopiumSystem/ShopiumSystem.java
+++ src/shopiumSystem/ShopiumSystem.java
@@ -4,17 +4,51 @@ import java.util.ArrayList;
 
 public class ShopiumSystem {
 	private ArrayList<Item> items;
+	private ArrayList<Item> searchedResults;
+	private ArrayList<Item> cartItems;
+	private int searchedAmount;
 	
 	public ShopiumSystem() {
 		items = new ArrayList<Item>();
+		searchedResults = new ArrayList<Item>();
+		cartItems = new ArrayList<Item>();
 	}
 	
 	public void addItem(Item item) {
 		items.add(item);
 	}
 	
+	public void addSearchedItem(Item item) {
+		searchedResults.add(item);
+	}
+	
+	public void addCartItem(Item item) {
+		cartItems.add(item);
+	}
+	
+	public void searchItem(String itemName) {
+		int number = 1;
+		for(int i = 0; i < items.size(); i++) {
+			if(items.get(i).getName().toLowerCase().contains(itemName.toLowerCase())) {
+				System.out.printf("%3d) %s%n", number, items.get(i).getName());
+				addSearchedItem(items.get(i));
+				number++;
+			}
+		}
+		searchedAmount = number;
+		System.out.printf("%3d) %s%n", number, "Go to main menu");
+	}
+	
 	public ArrayList<Item> getItems(){
 		return items;
+	}
+	
+	public ArrayList<Item> getSearchedResults(){
+		return searchedResults;
+	}
+	
+	public ArrayList<Item> getCartItems(){
+		return cartItems;
 	}
 	
 }
